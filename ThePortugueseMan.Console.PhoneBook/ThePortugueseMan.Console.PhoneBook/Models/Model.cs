ca396869409bbe94a9378e6de0314e6d546c04f0
ThePortugueseMan <cemartins99@gmail.com>
2023-04-12 10:47:34 +0100

    TestApp

---
+++ ThePortugueseMan.Console.PhoneBook/ThePortugueseMan.Console.PhoneBook/Models/Model.cs
@@ -0,0 +1,26 @@
+﻿using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+
+public class BloggingContext : DbContext
+{
+    public DbSet<Contact> Contacts { get; set; }
+
+    public string DbPath { get; }
+
+    public BloggingContext()
+    {
+        DbPath = "UHLAHLAH;Initial Catalog=PhoneBook;Integrated Security=True;TrustServerCertificate = True;";
+    }
+
+    protected override void OnConfiguring(DbContextOptionsBuilder options)
+        => options.UseSqlServer($"Data Source={DbPath}");
+}
+
+
+public class Contact
+{
+    public int Id { get; set; }
+    public string? Name { get; set; }
+    public string? PhoneNumber { get; set; }
+}
---
+++ ThePortugueseMan.Console.PhoneBook/ThePortugueseMan.Console.PhoneBook/Program.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Linq;
+using System.Reflection.Metadata;
+
+using var db = new BloggingContext();
+
+// Note: This sample requires the database to be created before running.
+Console.WriteLine($"Database path: {db.DbPath}.");
+
+// Create
+Console.WriteLine("Inserting a new blog");
+db.Add(new Contact { 
+    Name = "Joao Pereira"
+    });
+db.SaveChanges();
+
+// Read
+Console.WriteLine("Querying for a blog");
+var contact = db.Contacts
+    .OrderBy(b => b.Name)
+    .First();
+
+// Update
+Console.WriteLine("Updating the blog and adding a post");
+contact.Name = "Johny Perao";
+contact.PhoneNumber = "910481257";
+db.SaveChanges();
+
+// Delete
+Console.WriteLine("Delete the blog");
+db.Remove(contact);
+db.SaveChanges();
